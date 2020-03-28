975811843ca584d11d8a7127f7ab50512ca77470
Felix Soest <felix@soest-net.de>
2020-03-28 23:34:18 +0100

    PES

---
+++ PES/Protkoll.py
@@ -0,0 +1,134 @@
+# Protokoll Photoelektronenspektroskopie
+# Schrittweite Winkel 0.3°
+# Minimum/Maximum --> k=0
+# Abzählen der Streifen, Spektrum 7 ist alpha = 0
+# Punkte auf positiver und negativer k-Achse
+# x-y Achse beschriften, Einheiten wichtig, Winkel umrechnen!
+# Punktwolke E(k) [ Nicht zu Linien verbinden], Spektrumbild mit Winkeln!
+# YbIr2Si2
+# 4f Schale
+# Yb 4f sehr leicht gebunden, Keine Bänder sondern Linien in der Bandstruktur, da schwach gebunden jedoch keine Bindung
+# Daten: Erste spalte Energieachse, Intensitäten verschoben
+
+# 10^-6 in einer Sekunde eine Monolage
+# Probe saubermachen
+# Laser - thermischer Prozess
+
+# Probe reinigen
+# Aufheizen durch Elektronenbeschuss
+# Hochschspannung und Glühkathode, 2000°C
+# Kohlenstoff 3700°C Schmelztemperatur
+# In Sauerstoff heizen, so Kohlenstoff oxidieren
+# --> Wolframoxid, dann heizen Wolframoxid verdampft bei 2200°C
+# Kohlenstoff in Wolframkristall --> diffundiert an die Oberfläche
+# Moment abpassen, wo Oxid weg, jedoch kein Kohlenstoff an Oberfläche
+# Abwechselnd Heizen, Abkühlen
+# Erst wenig heizen, dann progressiv zunehmen
+# Beugungsbilder
+
+# Ausprobieren von LEED-Software
+# Aufgaben stellen
+# Erklärung --> was passiert und wieso?
+# Druck Präparationskammer 11:25
+p = 1.76e-10 #mbar
+
+# Beschleunigungsspannung Glühwendel
+U = 1249 #V
+
+# Erhöhunhg der Temperatur der Glühwendel zur Emission
+I = 240 # mA
+# Druck nach erstem heizen 11:31
+p = 2.3e-9 #mbar
+
+# Sauerstoffeinlass 11:34
+p = 2.2e-6 #mbar
+
+# Heizen: 11:35-11:41
+U = 1000 #V
+I = 50 #mA
+
+# Druck nach heizen
+p = 2.32e-6 #mbar
+
+# Sauerstoffzufuhr beenden
+# Mit Titan-Getter-Pumpe Sauerstoff zu Festkörper 11:46-11:51
+
+# 11:52
+p = 1.16e-7 #mbar
+
+# 13:48
+p = 1.2e-9 #mbar
+
+# Überprüfung mittels LEED: kreuzförmige 'Schlieren'
+# erstes Heizen:
+U = 1000 #V
+I = 35 #ma
+# 1 Minute
+
+# zweites Heizen:
+U = 1000 #V
+I = 125 #ma
+# 1 Minute
+
+# drittes Heizen:
+U = 1250 #V
+I = 240 #ma
+# 1 Minute
+
+
+# Ausprobieren LEED-Software, Verkippung macht Beugungsbild fast unbrauchbar
+
+# Z Richtung Einstrahlrichtung
+
+# 1. Was passiert wenn weniger Atome?
+# 5,5,5
+# Intensität 0.05
+# Ekin = 222
+# Unscharf, da weniger Strahlen, damit weniger destruktive Interferenz
+# Weniger Atome, weniger Interferenzbedingungen
+
+
+# 2. Nur ein Atom?
+# Intensität 0.46
+# Ekin = 222
+# Keine Interferenz, dadurch Reflektrion in alle Richtungen / es Fehlt zweite Welle
+
+
+
+
+# 3. Kette von Atomen? (x,y,z Richtung?)
+# 31 Atome
+# x-Richtung
+# Intensität 0.011
+# Fourier-Trafo in eine Richtung
+# Mit Ewald Kugel argumentieren
+
+
+# z-Richtung
+# Intensität 0.022
+# Kein Nullreflex
+
+# Schriftlich, Bilder von Beugung und Realraum, Parameter des Bildes
+
+# 4. Stufenbild
+# Kippung l/r -6
+# I = 0.0
+
+# Stufenbild mit halber Stufenlänge
+# Man erhält extra Periodizität durch Stufen im Kristall
+
+# Genauer mathematischer Zusammenhang zwischen Stufenlänge und Beugungsbild
+
+# Was Stufe und nicht Kippung
+
+# Gegenkippung Stufe
+
+# Beugungsbild Stufen sehr gering --> Intensität hochstellen
+
+# Referenzbild mit zu viel Intensität und Stufenbild mit zu viel Intensität
+
+# Schräge Stufen --> Beugungsbild? Wieso?
+# Verkippung, Intensität hochstellen
+
+# Verkippung Richtung und Menge
+# Stufen?
